a92f9c6481c466a2b822b46a12608c6c40b4dc21
charan1805 <sreecharanmudhiraj@gmail.com>
2023-07-28 15:15:09 +0530

    First Class

---
+++ IDisplayable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assesment18
+{
+    internal interface IDisplayable
+    {
+        string Display();
+
+    }
+}
